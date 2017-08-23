c618342b15bf22bcccda9f6da3274f18c8c89e35
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-08-23 17:15:20 -0400

    added requirements for user-controller, added model to user.js, added default layout to view/layouts

---
+++ app/controller/users-controller.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const models = require('../models');
