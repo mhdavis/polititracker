13e53ebfd48f631f669d4ac1f356f0f15bb0aec7
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-09-04 13:12:49 -0400

    accounting for more possible conditions

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -42,7 +42,10 @@ let democracy = {
 
                 $('#' + id).append("<div class='card-footer text-center' id='" + idTwo + "'>" + "<a href='" + data.officials[i].urls[0] + "' target='_blank'><i class='profile-reps-social profile-reps-web fa fa-address-book fa-2x' aria-hidden='true'></i></a>")
 
+                if (!data.officials[i].channels) {
+                    $('#' + idTwo).append("Social Media N/A")
 
+                } else {
                 for (let j = 0; j < data.officials[i].channels.length; j++) {
 
                     switch (data.officials[i].channels[j].type) {
@@ -58,6 +61,8 @@ let democracy = {
                             $('#' + idTwo).append("<a href='http://www.youtube.com/user/" + data.officials[i].channels[j].id + "' target='_blank'><i class='profile-reps-social profile-reps-yt fa fa-youtube-play fa-2x' aria-hidden='true'></i></a>");
                             break;
                     }
+                }
+                    
                 }
 
                 idTwo++
@@ -75,18 +80,25 @@ let democracy = {
             contentType: "application/json;charset=utf-8"
 
         }).done(function(data) {
-            $(".profile-election-title").html(data.polititracker_elections[0].election.name)
-
-            $(".profile-election-date").append(data.polititracker_elections[0].election.electionDay)
-
             $(".profile-upcoming-elects").append("<span style='color: red;'>" + " " + data.polititracker_elections.length + "</span>");
 
-            $("#polling-location").html(data.polititracker_elections[0].pollingLocations[0].address.locationName)
+            if (data.polititracker_elections.length === 0) {
+                $(".profile-election-title").html("NO UPCOMING ELECTIONS")
 
-            $("#polling-address").html(data.polititracker_elections[0].pollingLocations[0].address.line1 + "<br>" + data.polititracker_elections[0].pollingLocations[0].address.city + ", " + data.polititracker_elections[0].pollingLocations[0].address.state + " " + data.polititracker_elections[0].pollingLocations[0].address.zip)
+            } else {
+                
+            $(".profile-election-title").html(data.polititracker_elections[0].election.name);
+
+            $(".profile-election-date").append(data.polititracker_elections[0].election.electionDay);
+
+            $("#polling-location").html(data.polititracker_elections[0].pollingLocations[0].address.locationName);
+
+            $("#polling-address").html(data.polititracker_elections[0].pollingLocations[0].address.line1 + "<br>" + data.polititracker_elections[0].pollingLocations[0].address.city + ", " + data.polititracker_elections[0].pollingLocations[0].address.state + " " + data.polititracker_elections[0].pollingLocations[0].address.zip);
 
             $("#polling-hours").html(data.polititracker_elections[0].pollingLocations[0].pollingHours);
 
+            $("#polling-times").html("Polling Hours:")
+            }
         })
     },
 
@@ -99,18 +111,19 @@ let democracy = {
             contentType: "application/json;charset=utf-8"
 
         }).done(function(data) {
+
 // data.polititracker_elections[0].contests.length
             for (let i = 0; i < 1; i++) {
                 $(".profile-contest-header").append("<h4><span class='profile-red'>Contest Type: </span>" + data.polititracker_elections[0].contests[i].type + "</h4><h4><span class='profile-red'>Office: </span>" + data.polititracker_elections[0].contests[i].office);
 
                 for (let j = 0; j < data.polititracker_elections[0].contests[i].candidates.length; j++) {
 
-                    $(".profile-cand-name").append(data.polititracker_elections[0].contests[0].candidates[j].name)
+                    $(".profile-cand-name").append(data.polititracker_elections[0].contests[i].candidates[j].name)
 
 
                     // If party exists post the URL, otherwise post N/A
                     if (data.polititracker_elections[0].contests[i].candidates[j].party === true) {
-                        $(".profile-cand-party").append(data.polititracker_elections[0].contests[0].candidates[j].party);
+                        $(".profile-cand-party").append(data.polititracker_elections[0].contests[i].candidates[j].party);
 
                     } else {
                         $(".profile-cand-party").append("N/A")
@@ -120,31 +133,31 @@ let democracy = {
 
                     // If website exists post the URL, otherwise post N/A
                     if (data.polititracker_elections[0].contests[i].candidates[j].candidatesUrl === true) {
-                        $(".profile-cand-url").append("<a href='" + data.polititracker_elections[0].contests[0].candidates[j].candidatesUrl + "'>Website</a>")
+                        $(".profile-cand-url").append("<a href='" + data.polititracker_elections[0].contests[i].candidates[j].candidatesUrl + "'>Website</a>")
 
                     } else {
                         $(".profile-cand-url").append('N/A')
 
                     }
 
-                        if (!data.polititracker_elections[0].contests[0].candidates[j].channels) {
+                        if (!data.polititracker_elections[0].contests[i].candidates[j].channels) {
                             $(".profile-cand-social").html("N/A")
 
                         } else {
 
-                    for (let k = 0; k < data.polititracker_elections[0].contests[0].candidates[j].channels.length; k++) {
+                    for (let k = 0; k < data.polititracker_elections[0].contests[i].candidates[j].channels.length; k++) {
 
                             switch (data.officials[i].channels[j].candidates[j].channels[k]) {
                                 case "Facebook":
-                                    $('#' + idTwo).append("<a href='http://www.facebook.com/" + data.polititracker_elections[0].contests[0].candidates[j].channels[k].id + "' target='_blank'><i class='profile-reps-social profile-reps-fb fa fa-facebook-square fa-2x' aria-hidden='true'></i></a>");
+                                    $('#' + idTwo).append("<a href='http://www.facebook.com/" + data.polititracker_elections[0].contests[i].candidates[j].channels[k].id + "' target='_blank'><i class='profile-reps-social profile-reps-fb fa fa-facebook-square fa-2x' aria-hidden='true'></i></a>");
                                     break;
 
                                 case "Twitter":
-                                    $('#' + idTwo).append("<a href='http://www.twitter.com/" + data.polititracker_elections[0].contests[0].candidates[j].channels[k].id + "' target='_blank'><i class='profile-reps-social profile-reps-tw fa fa-twitter-square fa-2x' aria-hidden='true'></i></a>");
+                                    $('#' + idTwo).append("<a href='http://www.twitter.com/" + data.polititracker_elections[0].contests[i].candidates[j].channels[k].id + "' target='_blank'><i class='profile-reps-social profile-reps-tw fa fa-twitter-square fa-2x' aria-hidden='true'></i></a>");
                                     break;
 
                                 case "YouTube":
-                                    $('#' + idTwo).append("<a href='http://www.youtube.com/user/" + data.polititracker_elections[0].contests[0].candidates[j].channels[k].id + "' target='_blank'><i class='profile-reps-social profile-reps-yt fa fa-youtube-play fa-2x' aria-hidden='true'></i></a>");
+                                    $('#' + idTwo).append("<a href='http://www.youtube.com/user/" + data.polititracker_elections[0].contests[i].candidates[j].channels[k].id + "' target='_blank'><i class='profile-reps-social profile-reps-yt fa fa-youtube-play fa-2x' aria-hidden='true'></i></a>");
                                     break;
                             }
                         }
